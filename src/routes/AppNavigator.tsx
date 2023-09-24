bd0af86c7687bd61c9e66597f81631ad1facc9ad
Camila Escobedo <camila.escobedo.f@mail.pucv.cl>
2023-09-24 17:34:52 -0300

    feat(Home): en desarrollo, versión 2.

--- src/routes/AppNavigator.tsx
+++ src/routes/AppNavigator.tsx
@@ -11,7 +11,7 @@ const Stack = createStackNavigator<RootStackParamList>();
 const AppNavigator: React.FC = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Auth" screenOptions={{ headerShown: false }}>
+            <Stack.Navigator initialRouteName="Main" screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Auth" component={Login} />
                 <Stack.Screen name="Register" component={Register} />
                 <Stack.Screen name="Main" component={TabGroup}/>
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { View, Text, Image, TextInput, FlatList, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 
 const HomeScreen: React.FC = () => {
+
   const usuario = {
     nombre: 'User',
-    foto: require('../../assets/iconos/usericon.png'),
+    foto: require('../../assets/iconos/UserProfile.png'),
   };
 
   const trabajosDestacados = [
@@ -13,8 +14,6 @@ const HomeScreen: React.FC = () => {
     { id: '3', titulo: 'Trabajo 3', imagen: require('../../assets/iconos/ImageReferencia.png') },
     { id: '4', titulo: 'Trabajo 4', imagen: require('../../assets/iconos/ImageReferencia.png') },
     { id: '5', titulo: 'Trabajo 5', imagen: require('../../assets/iconos/ImageReferencia.png') },
-    { id: '6', titulo: 'Trabajo 6', imagen: require('../../assets/iconos/ImageReferencia.png') },
-    { id: '7', titulo: 'Trabajo 7', imagen: require('../../assets/iconos/ImageReferencia.png') },
   ];
 
   const categoriasDestacadas = [
@@ -24,31 +23,36 @@ const HomeScreen: React.FC = () => {
     { id: '4', titulo: 'Categoría 4', imagen: require('../../assets/iconos/ImageReferencia.png') },
   ];
 
+  const serviceIcon = require('../../assets/iconos/Work.png');
+  const searchIcon = require('../../assets/iconos/Search.png');
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.container}>
 
         {/* Tarjeta superior */}
-        <View style={styles.tarjeta}>
+        <View style={styles.tarjetaSuperior}>
           <View style={styles.fila}>
             <Image source={require('../../assets/seaJoblogo.png')} style={styles.logo} />
             <Text style={styles.saludo}>Hola {usuario.nombre}!</Text>
           </View>
           <View style={styles.fila}>
-          <Image source={usuario.foto} style={styles.fotoPerfil} />
-            <View>
-              <Text>Ganancias de dinero</Text>
-              <Text>39.000 CLP</Text>
+            <Image source={usuario.foto} style={styles.fotoPerfil} />
+            <View style={styles.gananciasContainer}>
+              <Text style={styles.gananciaTexto}>Ganancias de dinero</Text>
+              <Text style={styles.gananciaNumero}>39.000 CLP</Text>
             </View>
           </View>
         </View>
 
         {/* Buscador */}
-        <View style={styles.tarjeta}>
-          <View style={styles.buscadorContainer}>
-            <Image source={{ uri: 'URL_ICONO_LUPA' }} style={styles.iconoLupa} />
-            <TextInput placeholder="¿Qué buscas?" style={styles.buscador} />
-          </View>
+        <View style={styles.buscadorContainer}>
+          <TextInput 
+            placeholder="¿Qué buscas?" 
+            style={styles.buscadorTexto} 
+            placeholderTextColor="#AEBFFB"
+          />
+          <Image source={searchIcon} style={styles.iconoLupa} />
         </View>
 
         {/* Trabajos destacados */}
@@ -63,6 +67,7 @@ const HomeScreen: React.FC = () => {
               </View>
             )}
             keyExtractor={(item) => item.id}
+            scrollEnabled={false} // Para deshabilitar el scroll
           />
         </View>
 
@@ -79,17 +84,22 @@ const HomeScreen: React.FC = () => {
             )}
             keyExtractor={(item) => item.id}
             numColumns={2} // Mostrar en 2 columnas
+            scrollEnabled={false} // Para deshabilitar el scroll
           />
         </View>
 
         {/* Ve los ultimos trabajos */}
         <TouchableOpacity style={styles.tarjetaUltimosTrabajos} onPress={() => {
-          // Aquí agregar la lógica para manejar el clic
           console.log('Tarjeta clickeada');
         }}>
-          <Text style={styles.saludoUltimosTrabajos}>¡Ve los últimos servicios publicados!</Text>
-          <Image source={{ uri: 'URL_ICONO_LUPA' }} style={styles.iconoUltimosTrabajos} />
+          <Text style={styles.saludoUltimosTrabajos}>
+            ¡Ve los últimos {"\n"}
+            servicios {"\n"}
+            publicados!
+          </Text>
+          <Image source={serviceIcon} style={{ ...styles.iconoUltimosTrabajos, tintColor: 'white' }} />
         </TouchableOpacity>
+
 
       </View>
     </ScrollView>
@@ -98,64 +108,122 @@ const HomeScreen: React.FC = () => {
 };
 
 const styles = StyleSheet.create({
+
+  // ESTILOS GENERALES
+
   container: {
     padding: 16,
-    paddingVertical: 40
+    paddingVertical: 40,
+    backgroundColor: '#FFFFFF',
   },
   tarjeta: {
     padding: 16,
-    backgroundColor: '#FFF',
+    backgroundColor: '#F3F6FF',
     borderRadius: 8,
     marginBottom: 16,
   },
+
+  // TARJETA SUPERIOR
+
+  tarjetaSuperior: {
+    padding: 16,
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    marginBottom: 16,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 6,
+  },
+
   fila: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     marginBottom: 8,
   },
+
   logo: {
     width: 150,
     height: 30,
     resizeMode: 'contain',
   },
+
   saludo: {
     fontSize: 18,
     fontWeight: 'bold',
   },
+
   fotoPerfil: {
-    width: 100,
-    height: 100,
-    borderRadius: 25,
-    resizeMode: 'contain',
+    width: 110,
+    height: 110,
+    resizeMode: 'cover',
   },
+
+  gananciasContainer: {
+    marginRight: 16,
+  },
+
+  gananciaTexto: {
+    fontSize: 15,
+    fontWeight: '700',
+    lineHeight: 16,
+    letterSpacing: 0,
+    textAlign: 'left',
+    color: '#0182AB',
+    marginBottom: 8,
+  },
+
+  gananciaNumero: {
+    fontSize: 30,
+    fontWeight: '400',
+    lineHeight: 30,
+    letterSpacing: 0,
+    textAlign: 'left',
+    color: '#47AE64', // Puedes ajustar el tono de verde según prefieras
+  },
+
+  // BUSCADOR
+
   buscadorContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#F3F3F3',
-    borderRadius: 8,
-    padding: 8,
+    backgroundColor: '#F3F6FF',
+    borderRadius: 50,
+    marginBottom: 16, // Agregamos un margen inferior para separarlo de los siguientes elementos
+    paddingHorizontal: 30,
+    paddingVertical: 10
   },
+
   iconoLupa: {
-    width: 24,
-    height: 24,
-    marginRight: 8,
+    width: 28,
+    height: 28,
   },
-  buscador: {
+
+  buscadorTexto: {
+    fontSize: 16,
     flex: 1,
   },
+
+  // TRABAJOS DESTACADOS
+
   tituloTrabajos: {
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 8,
   },
+
   tarjetaTrabajo: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#F3F3F3',
-    borderRadius: 8,
+    backgroundColor: '#FFF',
     padding: 16,
-    marginBottom: 8,
+    marginBottom: 20,
+    elevation: 4,
   },
   imagenTrabajo: {
     width: 50,
@@ -163,12 +231,16 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginRight: 16,
   },
+
+  // CATEGORIAS DESTACADAS
+
   tarjetaCategoria: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#F3F3F3',
+    backgroundColor: '#FFF',
     borderRadius: 8,
     margin: 8,
+    elevation: 4,
   },
   imagenCategoria: {
     width: '100%',
@@ -180,23 +252,35 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontWeight: 'bold',
   },
+
+  // VER ULTIMOS TRABAJOS
+
   tarjetaUltimosTrabajos: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     padding: 16,
-    backgroundColor: '#FFF',
+    backgroundColor: '#66AFFF', // Color de fondo de la tarjeta
     borderRadius: 8,
-    marginBottom: 16,
+    marginBottom: 20,
+    elevation: 6,
   },
+
   saludoUltimosTrabajos: {
-    fontSize: 18,
+    fontSize: 28,
     fontWeight: 'bold',
+    color: 'white', // Color de la letra
+    flex: 1, // Asegura que el texto ocupe todo el espacio disponible
+    flexWrap: 'wrap', // Permite que el texto se envuelva
   },
+
   iconoUltimosTrabajos: {
-    width: 24,
-    height: 24,
+    width: 82,
+    height: 82,
+    marginRight: 16,
+    tintColor: 'white', // Color del icono
   },
+
 });
 
 export default HomeScreen;
