29ce398e668fc40be8135480a726ea64ad461654
seba <sebastianfv@live.com>
2023-10-21 14:20:23 -0300

    Cambios tarjetas de buscardor

--- src/components/TarjetaServicioYPerfil.tsx
+++ src/components/TarjetaServicioYPerfil.tsx
@@ -17,30 +17,51 @@ const TarjetaServicioYPerfil: React.FC<TarjetaProps> = ({ item, type }) => {
         const servicioItem = item as ServicioData;
 
         return (
-            <View style={styles.serviceCard}>
-                <View style={styles.imageColumn}>
+            <View style={{
+                flexDirection: 'row',
+                backgroundColor: '#EEF9FF',
+                borderRadius: 10,
+                padding: 10,
+                marginBottom: 20,
+                alignItems: 'center',  // Asegura que los elementos están centrados verticalmente
+            }}>
+                <View style={{ flex: 0.4, alignItems: "flex-start" }}>
+                    {/* Imagen del servicio */}
                     <Image
                         source={servicioItem.imagen && servicioItem.imagen !== '' ? { uri: servicioItem.imagen } : defaultImage}
-                        style={styles.image}
+                        style={{ width: 150, height: 150 ,borderRadius:10}}
+            resizeMode="contain" 
                     />
                 </View>
-                <View style={styles.infoColumn}>
-                    <Text style={[styles.serviceName, styles.textBold]} numberOfLines={2}>{servicioItem.nombreServicio}</Text>
-                    <Text style={styles.serviceDescription} numberOfLines={3}>{servicioItem.descripcion}</Text>
-    
-                    <View style={styles.iconRowContainer}>
-                        <View style={styles.iconRow}>
-                            <View style={styles.iconColumn}>
-                                <FontAwesome name="map-marker" size={16} color="#4593EE" />
-                                <FontAwesome name="calendar" size={16} color="#4593EE" style={styles.iconSpacing} />
-                            </View>
-                            <View style={styles.iconInfoColumn}>
-                                <Text style={styles.textBold}>{servicioItem.direccion}</Text>
-                                <Text style={styles.textBold}>{servicioItem.fechaSolicitud}</Text>
-                            </View>
-                        </View>
+                <View style={{ flex: 0.6, alignItems: "flex-start", justifyContent: 'space-between', marginStart:10 }}>  
+                    {/* texto nombre*/}
+                    <View style={{ alignItems: 'center', width: '100%', marginRight: 5}}> 
+                        <Text numberOfLines={1} style={{ fontWeight: 'bold', color: '#4593EE', textAlign: 'center' }}>
+                            {servicioItem.nombreServicio}
+                        </Text>
                     </View>
-                    <Text style={[styles.categoryTag]} numberOfLines={2}>{servicioItem.categoria}</Text>
+                    {/* texto descripcion*/}
+                    <View style={{ height: 50 ,marginRight: 1, marginTop:10}}>  
+                        <Text numberOfLines={2} style={{ color: "#4593EE" }}>
+                            {servicioItem.descripcion}
+                        </Text>
+                    </View>
+                    {/* Zona Direccion/ Fecha / Tag*/}
+                    <View>
+                        <Text numberOfLines={1} style={{ fontWeight: 'bold', color: '#4593EE', }}>
+                            <FontAwesome name="map-marker" size={16} color="#4593EE" /> {" "}
+                            {servicioItem.direccion}
+                        </Text>
+                        <Text numberOfLines={1} style={{ fontWeight: 'bold', color: '#4593EE', marginTop: 10 }}>
+                            <FontAwesome name="calendar" size={16} color="#4593EE" /> {" "}
+                            {servicioItem.fechaSolicitud}
+                        </Text>
+                        <Text numberOfLines={1} style={{ fontWeight: 'bold', color: '#4593EE', marginTop: 10 }}>
+                            <FontAwesome name="tag" size={16} color="#4593EE" /> {" "}
+                            {servicioItem.categoria}
+                        </Text>
+                    </View>
+            
                 </View>
             </View>
         );
@@ -52,16 +73,36 @@ const TarjetaServicioYPerfil: React.FC<TarjetaProps> = ({ item, type }) => {
             usuarioItem.apellidoMaterno || ''
         ].join(' ').trim();
         return (
-            <View style={styles.profileCard}>
-                <View style={styles.imageColumn}>
+            <View style={{
+                flexDirection: 'row',
+                backgroundColor: '#EEF9FF',
+                borderRadius: 20,
+                padding: 10,
+                marginBottom: 20,
+                  // Asegura que los elementos están centrados verticalmente
+            }}>
+                <View style={{ flex: 0.4, alignItems: "flex-start" }}>
+                    {/* Imagen del servicio */}
                     <Image
                         source={usuarioItem.imagenDePerfil && usuarioItem.imagenDePerfil !== '' ? { uri: usuarioItem.imagenDePerfil } : defaultImage}
-                        style={styles.image}
+                        style={{ width: 150, height: 150 ,borderRadius:10}}
+            resizeMode="contain" 
                     />
                 </View>
-                <View style={styles.infoColumn}>
-                    <Text style={[styles.profileName, styles.textBold]} numberOfLines={2}>{nombreCompleto}</Text>
-                    <Text style={[styles.profileDescription]} numberOfLines={5}>Esta es una descripción de ejemplo ❤️, soy una persona muy entusiasmada por trabajar ⭐, denme pega por favor 😥, haré bien mi trabajo LO PROMETO LO JURO.</Text>
+                <View style={{ flex: 0.6, alignItems: "flex-start", marginRight: 1, marginTop:20 ,marginStart:10}}>  
+                    {/* texto nombre*/}
+                    <View style={{ alignItems: 'center', width: '100%' ,marginBottom:15}}> 
+                        <Text numberOfLines={1} style={{ fontWeight: 'bold', color: '#4593EE', textAlign: 'center' }}>
+                            {usuarioItem.nombre} {usuarioItem.apellidoPaterno} {usuarioItem.apellidoMaterno}
+                        </Text>
+                    </View>
+                    {/* texto descripcion*/}
+                    <View >  
+                        <Text numberOfLines={8} style={{ color: "#4593EE" ,marginRight: 5,marginBottom:10}}>
+                            <FontAwesome name="id-card-o" size={16} color="#4593EE" /> {" "}{usuarioItem.descripcion}
+                        </Text>
+                    </View>
+            
                 </View>
             </View>
         );
@@ -69,10 +110,7 @@ const TarjetaServicioYPerfil: React.FC<TarjetaProps> = ({ item, type }) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-    },
+    
     scrollToTopButton: {
         position: 'absolute',
         right: 10,
@@ -85,111 +123,8 @@ const styles = StyleSheet.create({
         color: '#ffffff',
         fontSize: 20,
     },
-    serviceCard: {
-        flexDirection: 'row',
-        backgroundColor: '#EEF9FF',
-        marginHorizontal: 15,
-        borderRadius: 10,
-        padding: 5,
-        paddingBottom: 15,
-        paddingTop: 15,
-        marginBottom: 10,
-        justifyContent: 'space-between',
-        height: 250, 
-    },
-    profileCard: {
-        flexDirection: 'row',
-        backgroundColor: '#EEF9FF',
-        marginHorizontal: 15,
-        borderRadius: 10,
-        padding: 5,
-        paddingBottom: 15,
-        paddingTop: 15,
-        marginBottom: 10,
-        justifyContent: 'space-between',
-        height: 250,
-    },
-    imageColumn: {
-        width: '40%',
-        height: '100%', 
-        marginLeft: 5
-
-    },
-    image: {
-        width: '100%',
-        height: '100%', 
-        resizeMode: 'cover',
-    },
-    infoColumn: {
-        width: '60%',
-        height: '100%', 
-        paddingHorizontal: 10,
-        justifyContent: 'center', // Centra el contenido verticalmente
-    },
-    serviceName: {
-        fontSize: 18,
-        color: '#4593EE',
-        height: 40,
-        textAlign: 'center',
-        overflow: 'hidden',
-    },
-    serviceDescription: {
-        marginVertical: 10,
-        height: 70,
-        color: '#4593EE',
-        textAlign: 'justify',
-        overflow: 'hidden',
-    },
-    iconRowContainer: {
-        flex: 1,
-        justifyContent: 'flex-end',
-    },
-    iconRow: {
-        flexDirection: 'row',
-    },
-    iconColumn: {
-        width: '13%',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    iconInfoColumn: {
-        width: '80%',
-        justifyContent: 'space-between',
-    },
-    textBold: {
-        fontWeight: 'bold', 
-        color: '#4593EE',
-    },
-    iconSpacing: {
-        marginTop: 20,
-    },
-    categoryTag: {
-        fontSize: 12,
-        backgroundColor: '#8DBEEF',
-        color: 'white',
-        textAlign: 'center',
-        overflow: 'hidden',
-        borderRadius: 10,
-        paddingVertical: 5,
-        marginTop: 10,
-        alignSelf: 'center',
-        paddingHorizontal: 10,
-        fontWeight: "500",
-    },
-    profileName: {
-        fontSize: 18,
-        color: '#4593EE',
-        textAlign: 'center',
-        overflow: 'hidden',
-        marginBottom: 5, // Reduce el margen inferior
-    },
-
-    profileDescription: {
-        marginTop: 5, // Reduce el margen superior
-        color: '#4593EE',
-        textAlign: 'justify',
-        overflow: 'hidden',
-    },
+    
+    
 
 });
 
