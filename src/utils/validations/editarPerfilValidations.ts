526a3ecbf00dd5bb5561936aebed250f1c6f49e3
Camila Escobedo <camila.escobedo.f@mail.pucv.cl>
2023-11-11 14:18:51 -0300

    fix(editarPerfilValidation): telefono

--- src/utils/validations/editarPerfilValidations.ts
+++ src/utils/validations/editarPerfilValidations.ts
@@ -5,5 +5,8 @@ export const editarPerfilSchema = Yup.object().shape({
     apellidoPaterno: Yup.string().required('El apellido paterno es requerido').max(20, "El apellido paterno es demasiado largo"),
     apellidoMaterno: Yup.string().required('El apellido materno es requerido').max(20, "El apellido materno es demasiado largo"),
     descripcion: Yup.string().required('La descripción es requerida'),
-    telefono: Yup.string().required('El número telefónico es requerido').matches(/^[0-9]+$/, 'Número inválido'),
+    telefono: Yup.string()
+        .required("El número de teléfono es requerido.")
+        .matches(/^[0-9]+$/, "El número de teléfono solo debe contener números.")
+        .min(9, "El número de teléfono debe tener al menos 9 dígitos."),
 });
